e9e2cad6f490ae803282f62fd6d4d3c5971270ad
Fellipe Lima <140414505+francafellipe@users.noreply.github.com>
2023-08-18 11:10:59 -0300

    Add files via upload

---
+++ aula1python.py
@@ -0,0 +1,24 @@
+'''lista = [1,'dois',2.5,True]
+print 
+
+lista_numerica = [1,2,3,4,5,6,7,8,9,10]
+
+lista_numerica [3] = "Jas"
+
+print (lista_numerica)
+
+print (lista_numerica[5])
+
+print (lista_numerica[1:7:1])
+
+print (lista_numerica[:4])
+
+print (lista_numerica[4:])
+
+print (lista_numerica[7:0:-1])'''
+
+lista_letras = ["a", 'b', 'c', 'd', 'e' , 'f']
+
+for i in lista_letras:
+    if i not in ['c','d','e']:
+        print(i)
