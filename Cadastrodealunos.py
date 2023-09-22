e647ef88ecada7e7e421c798b4987572606a427c
Fellipe Lima <140414505+francafellipe@users.noreply.github.com>
2023-09-22 17:48:31 +0000

    Update Prova Cadastrodealunos.py

--- Cadastrodealunos.py
+++ Cadastrodealunos.py
@@ -1,25 +1,39 @@
+#funções 
+def cadastrar ():
+    nome = input('Nome : ')
+    matricula = input('Digite a matricula do aluno :')
+    alunos[matricula]=nome.lower()
+def remover ():
+    matricula = input('Digite o número da matricula :')
+    alunos.pop(matricula)
+
+def visualizar ():
+    print("Lista de alunos:")
+    for matricula, nome in alunos.items():
+        print(f'Matrícula: {matricula}\n '
+              f'Nome: {nome}')
 #var
+
 alunos = {}
-matricula = 101
+
 #inicio
 while True:
     escolha = int(input('Escolha uma opção :\n'
                         '1 - Cadastrar novo aluno ?\n'
-                        '2 - Dados do aluno : \n'
-                        '3 - Finalizar cadastros .\n'))
+                        '2 - Visualizar dados dos alunos : \n'
+                        '3 - Apagar aluno cadastrado :\n'
+                        '0 - Finalizar cadastros .\n'))
     match escolha:
         case 1 :
-            nome = input('Nome : ')
-            idade = input('Idade: ')
-            sexo = input ('Sexo : ')
-            matricula +=1
-            alunos[nome.lower]=idade , sexo.lower(), matricula
+            cadastrar()
 
         case 2 :
-            aluno = input('De qual aluno quer saber os dados ? ')
-            print (f'O {aluno} tem {[idade]} anos , é do sexo : {[sexo]} e a matricula é : {[matricula]} .')
-
+            visualizar()
+        
         case 3 :
+            remover()
+
+        case 0 :
             print('Cadastro de alunos finalizado !')
             break
         
